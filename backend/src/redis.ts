885f9bcdce725235eb7613ebff19496007e20a5a
Totoratsu <mayor.daniel.18@gmail.com>
2021-01-17 16:12:13 -0500

    Redis connection bugfix

--- backend/src/redis.ts
+++ backend/src/redis.ts
@@ -1,3 +1,15 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis();
+const {
+    REDIS_PORT = '6379',
+    REDIS_HOST = '127.0.0.1',
+    REDIS_PASS = 'secret4redis',
+} = process.env;
+
+export const redis = new Redis({
+    port: parseInt(REDIS_PORT),
+    host: REDIS_HOST,
+    family: 4,
+    password: REDIS_PASS,
+    db: 0,
+});
