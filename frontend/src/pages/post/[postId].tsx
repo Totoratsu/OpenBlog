6a9f848dc894f42febcd83d73944d3e12a6d04f2
Totoratsu <mayor.daniel.18@gmail.com>
2021-01-18 14:53:20 -0500

    Travis bugfix & Added Screenshots

--- frontend/src/pages/post/[postId].tsx
+++ frontend/src/pages/post/[postId].tsx
@@ -1,5 +1,6 @@
 import { gql } from 'graphql-request';
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import Markdown from 'react-markdown';
 
 import MainContainer from '../../components/MainContainer';
@@ -10,8 +11,19 @@ const PostPage = ({ post }: { post: IPost }): JSX.Element => {
 	return (
 		<MainContainer>
 			<h1 className="text-center">{post.title}</h1>
-			<p className="text-center text-muted">{post.description}</p>
-			<Markdown escapeHtml={true} source={post.content} />
+			<Row className="justify-content-center">
+				<Col md={6}>
+					<p className="text-center text-muted">{post.description}</p>
+				</Col>
+			</Row>
+			<Row className="justify-content-center">
+				<Col md={9}>
+					<Markdown escapeHtml={true} source={post.content} />
+				</Col>
+			</Row>
+
+			<br />
+			<p className="text-muted">Author: {post.author.username}</p>
 		</MainContainer>
 	);
 };
